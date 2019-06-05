7a042bc884528a321b0386df0f6f5984f719e2b1
Hamza JGUERIM <hamzajg@gmail.com>
2019-06-05 11:43:31 +0100

    Merging Remote..

---
+++ S.O.L.I.D_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S.O.L.I.D_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
